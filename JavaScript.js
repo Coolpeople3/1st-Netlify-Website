e37a9e7d59ede225289cdb3b0cd3f3efdd7e0b1b
HitarthYagnik <159194319+Coolpeople3@users.noreply.github.com>
2024-11-19 15:04:16 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,4 @@
 function startGame(gameName) {
-  alert(`Starting the ${gameName} game!`);
-
   if (gameName === "clicker") {
     loadClickerGame();
   } else if (gameName === "ticTacToe") {
@@ -12,9 +10,9 @@ function startGame(gameName) {
 
 function loadClickerGame() {
   document.body.innerHTML = `
-    <div style="text-align: center; margin-top: 50px;">
+    <div style="text-align: center; margin-top: 50px; color: white;">
       <h2>Clicker Game</h2>
-      <p>Click as fast as you can!</p>
+      <p>Click the button as many times as you can!</p>
       <button id="clickButton">Click Me!</button>
       <p>Clicks: <span id="clickCount">0</span></p>
       <button onclick="location.reload()">Back to Menu</button>
@@ -30,9 +28,19 @@ function loadClickerGame() {
 
 function loadTicTacToe() {
   document.body.innerHTML = `
-    <div style="text-align: center; margin-top: 50px;">
+    <div style="text-align: center; margin-top: 50px; color: white;">
       <h2>Tic-Tac-Toe</h2>
-      <p>Coming soon...</p>
+      <p>(This is a simple 2-player version)</p>
+      <div id="ticTacToeBoard" style="display: grid; grid-template-columns: repeat(3, 100px); gap: 5px; margin: 20px auto;">
+        ${Array(9)
+          .fill(null)
+          .map(
+            (_, index) =>
+              `<button class="tic-tac-toe-cell" onclick="makeMove(${index})" style="width: 100px; height: 100px; font-size: 24px;"></button>`
+          )
+          .join("")}
+      </div>
+      <p id="gameStatus">Player X's turn</p>
       <button onclick="location.reload()">Back to Menu</button>
     </div>
   `;
@@ -40,8 +48,9 @@ function loadTicTacToe() {
 
 function loadMemoryGame() {
   document.body.innerHTML = `
-    <div style="text-align: center; margin-top: 50px;">
+    <div style="text-align: center; margin-top: 50px; color: white;">
       <h2>Memory Game</h2>
+      <p>(Match the cards!)</p>
       <p>Coming soon...</p>
       <button onclick="location.reload()">Back to Menu</button>
     </div>
